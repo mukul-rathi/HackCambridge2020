b556fdacdaae5a89730a41b4730867a1b7092a84
michaelng126 <michaelng126@users.noreply.github.com>
2020-01-18 23:35:29 +0000

    Changed Computer Vision to Custom Vision. Now works with trained model online. Updated the connection from back end to front end - sends object type and its probability. Tested and works quite well.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import requests, os, sys, base64
 from flask import Flask, render_template, request, jsonify
 
-subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
-endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
-ocr_url = endpoint + "vision/v2.1/ocr"
+endpoint = "https://hackcambridge2020ecoscancustom.cognitiveservices.azure.com/customvision/v3.0/Prediction/b22c88e7-f18f-48a5-a47d-1fc90cf12440/classify/iterations/Iteration1/image"
+subscription_key = "d0852ee0061e46c4ba82fe724ddfbf1a"
 
 app = Flask(__name__)
 
@@ -21,22 +20,15 @@ def check_results():
     body = request.get_json()
     image_bytes = base64.b64decode(body['image_base64'].split(',')[1])
 
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
+    headers = {'Prediction-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
     params = {'language': 'unk', 'detectOrientation': 'true'}
-    response = requests.post(ocr_url, headers=headers, params=params, data=image_bytes)
+    response = requests.post(endpoint, headers=headers, params=params, data=image_bytes)
     response.raise_for_status()
 
     analysis = response.json()
 
-    # Extract the word bounding boxes and text.
-    line_infos = [region["lines"] for region in analysis["regions"]]
-    word_infos = []
-    for line in line_infos:
-        for word_metadata in line:
-            for word_info in word_metadata["words"]:
-                word_infos.append(word_info['text'])
+    predictions = analysis['predictions']
+    best_prediction = predictions[0]
 
-    print(analysis)
-    print(word_infos)
-    print('Hello')
-    return jsonify({'description' : ','.join(word_infos)})
+    print(best_prediction)
+    return jsonify({'object' : best_prediction['tagName'], 'probability': best_prediction['probability']})

601bbe35975586ca90094b81d450175bc21f069f
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-19 01:19:45 +0000

    Add camera to frontend

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,59 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react"
+import { Camera } from "react-cam"
+import axios from "axios"
 
-const IndexPage = () => (<h1>Eco Scan</h1>
+const useCaptureImage = initState => {
+  const [carbonFootprintData, setCarbonFootPrintData] = useState(initState)
+  const callAPI = imgSrc =>
+    axios
+      .post(
+        "process_image",
+        { "Content-Type": "application/json" },
+        JSON.stringify(imgSrc)
+      )
+      .then(res =>
+        setCarbonFootPrintData([
+          ...carbonFootprintData,
+          {
+            object: res.object,
+            probability: res.probability,
+          },
+        ])
+      )
+
+  return [carbonFootprintData, callAPI]
+}
+
+const IndexPage = () => {
+  const [carbonFootprintData, captureImage] = useCaptureImage([])
+
+  const cam = useRef(null)
+  const camera = useEffect(() => (
+    <Camera
+      showFocus={true}
+      front={false}
+      capture={captureImage}
+      ref={cam}
+      width="80%"
+      height="auto"
+      focusWidth="80%"
+      focusHeight="60%"
+      btnColor="white"
+    />
+  ))
+  return (
+    <>
+      <h1>Eco Scan</h1>
+      {camera}
+      <button onClick={img => cam.current.capture(img)}>Take image</button>
+      {carbonFootprintData.map(data => (
+        <div>
+          <span> {data.object}</span>
+          <span> {data.probability}</span>
+        </div>
+      ))}
+    </>
   )
+}
 
 export default IndexPage

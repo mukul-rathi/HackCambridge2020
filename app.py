81d3ba4b0517c2738270f66daa5a4f575f2c81a7
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-19 04:33:49 +0000

    Make Flask CORS compatible

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import requests, os, sys, base64
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 endpoint = "https://hackcambridge2020ecoscancustom.cognitiveservices.azure.com/customvision/v3.0/Prediction/b22c88e7-f18f-48a5-a47d-1fc90cf12440/classify/iterations/Iteration2/image"
 subscription_key = "d0852ee0061e46c4ba82fe724ddfbf1a"
 
 app = Flask(__name__)
+CORS(app)
 
 # The root route
 @app.route('/',methods=['POST'])
@@ -12,6 +14,7 @@ def check_results():
     # Get the JSON passed to the request and extract the image
     # Convert the image to a binary stream ready to pass to Azure AI services
     body = request.get_json()
+    print(body)
     image_bytes = base64.b64decode(body['image_base64'].split(',')[1])
 
     headers = {'Prediction-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
